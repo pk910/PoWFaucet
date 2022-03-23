5ff7a4d0d2353cf01a191b16716f1f7d0f8cc0b3
pk910 <philipp@pk910.de>
2022-03-23 01:55:46 +0100

    added default config

---
+++ README.md
@@ -0,0 +1,16 @@
+# PoWFaucet
+
+Proof of Work Faucet for EVM chains
+
+# Why
+
+Faucets for ETH Testnets are spammed by bots. This faucet tries to reduce the efficiency of these automated request by requiring some mining work to be done in exchange for ETH.
+
+# Run
+
+`npm install`
+
+`npm run start`
+
+# Configure
+
--- src/common/FaucetConfig.ts
+++ src/common/FaucetConfig.ts
@@ -4,14 +4,13 @@ import * as path from 'path';
 export interface IFaucetConfig {
   appBasePath: string;
   faucetVersion: string;
+  staticPath?: string;
+  faucetStore: string;
 
   faucetTitle: string;
   faucetImage: string;
   faucetLogFile: string;
-
   serverPorts: IFaucetPortConfig[];
-  staticPath?: string;
-  faucetStore: string;
 
   powShareReward: number;
   claimMinAmount: number;
@@ -64,19 +63,55 @@ export interface IFaucetHCaptchaConfig {
 export let faucetConfig: IFaucetConfig = (() => {
   var packageJson = require('../../package.json');
   var basePath = path.join(__dirname, "..", "..");
-  let baseConfig = {
+  let defaultConfig: IFaucetConfig = {
     appBasePath: basePath,
     faucetVersion: packageJson.version,
-
     staticPath: path.join(basePath, "static"),
     faucetStore: path.join(basePath, "faucet-store.json"),
+
     powPingInterval: 10,
     powPingTimeout: 30,
+    faucetTitle: "PoW Faucet",
+    faucetImage: "https://ligi.de/assets/img/fauceth_420.jpg",
+    faucetLogFile: null,
+    serverPorts: [
+      { port: 8080 }
+    ],
+    powShareReward:     25000000000000000,
+    claimMinAmount:    100000000000000000,
+    claimMaxAmount:  10000000000000000000,
+    powSessionTimeout: 3600,
+    claimSessionTimeout: 7200,
+    claimAddrCooldown: 7200,
+    powSessionSecret: "***insecure***",
+    powScryptParams: {
+      cpuAndMemory: 4096,
+      blockSize: 8,
+      paralellization: 1,
+      keyLength: 16,
+      difficulty: 9
+    },
+    powNonceCount: 2,
+    verifyLocalPercent: 10,
+    verifyLocalMaxQueue: 100,
+    verifyMinerPeerCount: 2,
+    verifyLocalLowPeerPercent: 100,
+    verifyMinerPercent: 100,
+    verifyMinerIndividuals: 2,
+    verifyMinerMissPenalty: 10000000000000000,
+    hcaptcha: null,
+    ethRpcHost: "http://127.0.0.1:8545/",
+    ethWalletKey: "fc2d0a2d823f90e0599e1e9d9202204e42a5ed388000ab565a34e7cbb566274b",
+    ethChainId: 1337802,
+    ethTxGasLimit: 500000,
+    ethTxMaxFee: 1800000000,
+    ethTxPrioFee: 800000000,
+    ethMaxPending: 12
   };
 
-  let configFlle = path.join(baseConfig.appBasePath, "faucet-config.json");
+  let configFlle = path.join(defaultConfig.appBasePath, "faucet-config.json");
   let configJson = fs.readFileSync(configFlle, "utf8");
-  return Object.assign(baseConfig, JSON.parse(configJson));
+  return Object.assign(defaultConfig, JSON.parse(configJson));
 })();
 
 
