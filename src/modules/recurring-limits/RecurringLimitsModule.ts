0f62bbdea289b1a3a4b7e53a45a5fa14b0262360
Konstantin Ryzhov <theaklair@gmail.com>
2024-07-29 12:14:11 +0500

    Run prettier on RecurringLimitsModule

--- src/modules/recurring-limits/RecurringLimitsModule.ts
+++ src/modules/recurring-limits/RecurringLimitsModule.ts
@@ -1,10 +1,17 @@
 import { ServiceManager } from "../../common/ServiceManager.js";
 import { EthWalletManager } from "../../eth/EthWalletManager.js";
-import {FaucetSession, ISessionStartUserInput} from "../../session/FaucetSession.js";
+import {
+  FaucetSession,
+  ISessionStartUserInput,
+} from "../../session/FaucetSession.js";
 import { BaseModule } from "../BaseModule.js";
 import { ModuleHookAction } from "../ModuleManager.js";
-import { defaultConfig, IRecurringLimitConfig, IRecurringLimitsConfig } from './RecurringLimitsConfig.js';
-import { FaucetError } from '../../common/FaucetError.js';
+import {
+  defaultConfig,
+  IRecurringLimitConfig,
+  IRecurringLimitsConfig,
+} from "./RecurringLimitsConfig.js";
+import { FaucetError } from "../../common/FaucetError.js";
 import { FaucetDatabase } from "../../db/FaucetDatabase.js";
 import { renderTimespan } from "../../utils/DateUtils.js";
 import { ISessionRewardFactor } from "../../session/SessionRewardFactor.js";
@@ -14,12 +21,20 @@ export class RecurringLimitsModule extends BaseModule<IRecurringLimitsConfig> {
 
   protected override startModule(): Promise<void> {
     this.moduleManager.addActionHook(
-      this, ModuleHookAction.SessionStart, 6, "Recurring limits check",
-      (session: FaucetSession, userInput: ISessionStartUserInput) => this.processSessionStart(session, userInput)
+      this,
+      ModuleHookAction.SessionStart,
+      6,
+      "Recurring limits check",
+      (session: FaucetSession, userInput: ISessionStartUserInput) =>
+        this.processSessionStart(session, userInput)
     );
     this.moduleManager.addActionHook(
-      this, ModuleHookAction.SessionRewardFactor, 6, "recurring limits factor",
-      (session: FaucetSession, rewardFactors: ISessionRewardFactor[]) => this.processSessionRewardFactor(session, rewardFactors)
+      this,
+      ModuleHookAction.SessionRewardFactor,
+      6,
+      "recurring limits factor",
+      (session: FaucetSession, rewardFactors: ISessionRewardFactor[]) =>
+        this.processSessionRewardFactor(session, rewardFactors)
     );
     return Promise.resolve();
   }
@@ -28,67 +43,89 @@ export class RecurringLimitsModule extends BaseModule<IRecurringLimitsConfig> {
     return Promise.resolve();
   }
 
-  private async processSessionStart(session: FaucetSession, _userInput: ISessionStartUserInput): Promise<void> {
-    if(session.getSessionData<string[]>("skip.modules", []).indexOf(this.moduleName) !== -1)
+  private async processSessionStart(
+    session: FaucetSession,
+    _userInput: ISessionStartUserInput
+  ): Promise<void> {
+    if (
+      session
+        .getSessionData<string[]>("skip.modules", [])
+        .indexOf(this.moduleName) !== -1
+    )
       return;
-    await Promise.all(this.moduleConfig.limits.map((limit) => this.checkLimit(session, limit)));
+    await Promise.all(
+      this.moduleConfig.limits.map((limit) => this.checkLimit(session, limit))
+    );
   }
 
-  private async checkLimit(session: FaucetSession, limit: IRecurringLimitConfig): Promise<void> {
+  private async checkLimit(
+    session: FaucetSession,
+    limit: IRecurringLimitConfig
+  ): Promise<void> {
     let remoteIP = session.getRemoteIP();
     const userId = session.getUserId();
-    if(limit.ip4Subnet && remoteIP.match(/^[0-9.]+$/)) {
+    if (limit.ip4Subnet && remoteIP.match(/^[0-9.]+$/)) {
       const ipParts = remoteIP.split(".").slice(0, limit.ip4Subnet / 8);
-      if(ipParts.length < 4) {
+      if (ipParts.length < 4) {
         ipParts.push("%");
         remoteIP = ipParts.join(".");
       }
     }
 
-    const finishedSessions = await ServiceManager.GetService(FaucetDatabase).getFinishedSessions({
-      targetAddr: session.getTargetAddr(), remoteIP, userId
-    },  limit.duration, true);
+    const finishedSessions = await ServiceManager.GetService(
+      FaucetDatabase
+    ).getFinishedSessions(
+      {
+        targetAddr: session.getTargetAddr(),
+        remoteIP,
+        userId,
+      },
+      limit.duration,
+      true
+    );
 
     let limitApplies = false;
-    if(limit.limitCount > 0 && finishedSessions.length >= limit.limitCount) {
+    if (limit.limitCount > 0 && finishedSessions.length >= limit.limitCount) {
       limitApplies = true;
-      if(!limit.action || limit.action === "block") {
-        const errMsg = limit.message || [
-          "You have already created ",
-          finishedSessions.length,
-          (finishedSessions.length > 1 ? " sessions" : " session"),
-          " in the last ",
-          renderTimespan(limit.duration)
-        ].join("");
-        throw new FaucetError(
-          "RECURRING_LIMIT",
-          errMsg,
-        );
-        }
-    }
-    if(limit.limitAmount > 0) {
-      let totalAmount = 0n;
-      finishedSessions.forEach((session) => totalAmount += BigInt(session.dropAmount));
-      if(totalAmount >= BigInt(limit.limitAmount)) {
-        limitApplies = true;
-        if(!limit.action || limit.action === "block") {
-          const errMsg = limit.message || [
-            "You have already requested ",
-            ServiceManager.GetService(EthWalletManager).readableAmount(totalAmount),
+      if (!limit.action || limit.action === "block") {
+        const errMsg =
+          limit.message ||
+          [
+            "You have already created ",
+            finishedSessions.length,
+            finishedSessions.length > 1 ? " sessions" : " session",
             " in the last ",
-            renderTimespan(limit.duration)
+            renderTimespan(limit.duration),
           ].join("");
-          throw new FaucetError(
-            "RECURRING_LIMIT",
-            errMsg,
-          );
+        throw new FaucetError("RECURRING_LIMIT", errMsg);
+      }
+    }
+    if (limit.limitAmount > 0) {
+      let totalAmount = 0n;
+      finishedSessions.forEach(
+        (session) => (totalAmount += BigInt(session.dropAmount))
+      );
+      if (totalAmount >= BigInt(limit.limitAmount)) {
+        limitApplies = true;
+        if (!limit.action || limit.action === "block") {
+          const errMsg =
+            limit.message ||
+            [
+              "You have already requested ",
+              ServiceManager.GetService(EthWalletManager).readableAmount(
+                totalAmount
+              ),
+              " in the last ",
+              renderTimespan(limit.duration),
+            ].join("");
+          throw new FaucetError("RECURRING_LIMIT", errMsg);
         }
       }
     }
 
-    if(limitApplies && typeof limit.rewards !== "undefined") {
+    if (limitApplies && typeof limit.rewards !== "undefined") {
       const cfactor = session.getSessionData("recurring-limits.factor");
-      if(typeof cfactor === "undefined" || limit.rewards < cfactor)
+      if (typeof cfactor === "undefined" || limit.rewards < cfactor)
         session.setSessionData("recurring-limits.factor", limit.rewards);
     }
   }
@@ -96,28 +133,33 @@ export class RecurringLimitsModule extends BaseModule<IRecurringLimitsConfig> {
   public async getTimeToNewSessionStart(userId: string): Promise<number> {
     const limit = this.moduleConfig.limits[0];
     if (!limit) {
-      throw new FaucetError(
-          "RECURRING_LIMIT",
-          'Limit is not set'
-      );
+      throw new FaucetError("RECURRING_LIMIT", "Limit is not set");
     }
-    const lastStartTime = await ServiceManager.GetService(FaucetDatabase).getLastFinishedSessionStartTime(userId,  limit.duration);
+    const lastStartTime = await ServiceManager.GetService(
+      FaucetDatabase
+    ).getLastFinishedSessionStartTime(userId, limit.duration);
     if (!lastStartTime) {
       return 0;
     }
     return lastStartTime * 1000 + limit.duration * 1000;
   }
 
-  private async processSessionRewardFactor(session: FaucetSession, rewardFactors: ISessionRewardFactor[]) {
-    if(session.getSessionData<string[]>("skip.modules", []).indexOf(this.moduleName) !== -1)
+  private async processSessionRewardFactor(
+    session: FaucetSession,
+    rewardFactors: ISessionRewardFactor[]
+  ) {
+    if (
+      session
+        .getSessionData<string[]>("skip.modules", [])
+        .indexOf(this.moduleName) !== -1
+    )
       return;
     const rewardPerc = session.getSessionData("recurring-limits.factor", 100);
-    if(rewardPerc !== 100) {
+    if (rewardPerc !== 100) {
       rewardFactors.push({
         factor: rewardPerc / 100,
         module: this.moduleName,
       });
     }
   }
-
 }
