051192b8dadc97de3ccb0ce69c55f26a207574eb
Konstantin Ryzhov <theaklair@gmail.com>
2024-08-09 17:24:52 +0500

    Create zodSchemaBodyValidation.ts

---
+++ src/utils/zodSchemaBodyValidation.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export function zodSchemaBodyValidation<Schema extends z.ZodObject<any, any>>(
+  body: Buffer,
+  schema: Schema
+): z.infer<Schema> {
+  const input = JSON.parse(body.toString("utf8"));
+  const parsedBody = JSON.parse(input as string);
+  const parsed = schema.safeParse(parsedBody);
+  if (parsed.success) {
+    return parsed.data as Required<z.infer<Schema>>;
+  } else {
+    throw new Error(parsed.error.errors[0].message);
+  }
+}
