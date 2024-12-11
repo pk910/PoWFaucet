35e3d475f60ca5234658432680db83768e4243a2
Anna Linnik <annielinnik@gmail.com>
2024-12-11 20:18:14 +0400

    send the faucet name as tag to sentry

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,18 +1,15 @@
 import * as Sentry from "@sentry/node";
 
 if (process.env.SENTRY_DSN) {
-  const env = process.env.APP_ENV || "development";
-  const fullEnvName = process.env.SENTRY_FAUCET_NAME
-    ? process.env.SENTRY_FAUCET_NAME + "-" + env
-    : env;
-
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    environment: fullEnvName,
+    environment: process.env.APP_ENV || "development",
     // Tracing
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
     // Set sampling rate for profiling - this is relative to tracesSampleRate
     profilesSampleRate: 1.0,
   });
+
+  Sentry.setTag("faucet", process.env.SENTRY_FAUCET_NAME);
 }
